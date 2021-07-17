d23aaac0de68ed9dfd7f8cfa019787fccaafc937
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-17 10:13:02 +0530

    Update heatmap.r

--- heatmap.r
+++ heatmap.r
@@ -1,39 +1,4 @@
-
-heat_map <- x_cleaned_2_
-
-
-
-
-#Month variable
-heat_map<-heat_map %>% 
-  group_by(month = month(Date, label = TRUE))
-
-##Weekd variable
-heat_map$weekd <- weekdays(heat_map$Date)
-
-##round time
-##convert to numeric and then remove
-
-heat_map$TIME <- as.numeric(heat_map$Time)
-
-heat_map <- heat_map %>%
-  filter( !is.na(TIME))
-
-heat_map
-
-
-heat_map <- heat_map %>%
-  filter( !is.na(Age))
-
-
-#ROUND OFF
-
-heat_map$TIME <- round(heat_map$TIME)
-
-
-heat_map <- heat_map %>% select(-Time)
-##-------------------------------------------------------------------
-
+##ggplot
 # Create a theme that is a bit more friendly. This isn't required to do the visualization,
 # but it's a painful heatmap to look at otherwise. We start with a minimalist theme... and
 # then basically make it even more minimalist. Thank you, Tufte and Few.
@@ -52,32 +17,9 @@ theme_heatmap <- theme_light() +                 # Start with a minimalist theme
 
 
 
+heat_map_t
 
-
-##More Cleaning
-
-
-
-# Group by the weekday, hour, and devicetype, and then summarise by getting the
-# mean of the visits.
-
-heat_map <- heat_map %>% select(-Age)
-
-
-install.packages("writexl")
-library("writexl")
-write_xlsx(heat_map,"E:\\heat_map.xlsx")
-
-##Manually cleaned it to 1
-
-heat_map_t <- heat_map %>% 
-  group_by(weekd, TIME) %>% count(Head) 
-
-
-
-##ggplot
-
-gg <- ggplot(heat_map_t, mapping = aes(x = weekd, y = TIME, 
+gg <- ggplot(heat_map_t, mapping = aes(x = weekday, y = TIME, 
                                            fill = n)) +
   geom_tile(colour="white") +  # This makes the heatmap (the colour is the lines in the grid)
   scale_fill_gradient(low = "#f4fff6", high="#0bb730") +  # The colour range to use
@@ -87,13 +29,13 @@ gg <- ggplot(heat_map_t, mapping = aes(x = weekd, y = TIME,
   labs(title = "Accidents by Day of Week / Hour of Day", y = "Hour of Day") +
   theme_heatmap  # Apply the theme defined earlier for styling
 
-
+gg
 gg_labeled <- gg +
   geom_text(aes(label = sprintf("%.1f", n)),  # sprintf formats to 2 decimals
             size = 3)                              # And we need to tweak the font size
 
 
-
+gg_labeled
 
 #Re-check
 
